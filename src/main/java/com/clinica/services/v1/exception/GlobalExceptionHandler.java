d4d960e976e8342144e92fb30d99584d138bdc05
Fernando Tapia <fernando.tapia.int@viaexperis2.pe>
2025-06-05 04:52:42 -0500

    config

--- src/main/java/com/clinica/services/v1/exception/GlobalExceptionHandler.java
+++ src/main/java/com/clinica/services/v1/exception/GlobalExceptionHandler.java
@@ -8,17 +8,14 @@ import com.clinica.services.v1.exception.exceptions.GenericException;
 import com.clinica.services.v1.exception.exceptions.ResourceNotFoundException;
 import com.clinica.services.v1.exception.exceptions.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 import org.springframework.core.codec.DecodingException;
 /*
     Qué hace:
